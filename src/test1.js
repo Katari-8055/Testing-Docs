9de00f4ed867cea9d5dcac86ff9c1274d51a38d0
Katari <kataricoder@gmail.com>
2026-03-09 01:31:04 +0530

    multiply

---
+++ src/test1.js
@@ -0,0 +1,15 @@
+/**
+ * Multiplies two numbers.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The result of multiplying a and b.
+ *
+ * @example
+ * multiply(2, 3); // returns 6
+ */
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = multiply;
